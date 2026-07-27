f585129e190b4b63dce149e6fadb03d433e4dffd
Dhruv Sharma <ds2420626@gmail.com>
2026-07-27 14:50:50 +0530

    Fix async pre-save password hashing hook in User model

--- server/models/User.js
+++ server/models/User.js
@@ -15,10 +15,6 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        'Please provide a valid email address',
-      ],
     },
     password: {
       type: String,
@@ -46,13 +42,12 @@ const userSchema = new mongoose.Schema(
 );
 
 // Encrypt password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Match password method
